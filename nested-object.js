aa7af98d6162b256d8a6aa31b372cf69347c7123
mdNaeemUddin <md.naeem_uddin98@yahoo.com>
2024-02-23 16:05:44 +0600

    practicing nested object in js

---
+++ nested-object.js
@@ -0,0 +1,20 @@
+// more objects : play with object 
+
+const data = [
+    { id: '01', name: 'naeem', age: 25 },
+    { id: '02', name: 'rubina', age: 23 }
+];
+
+console.log(data[1].name);
+
+
+const products = {
+    count: 5000,
+    data: [
+        { id: 1, name: 'dell laptop', price: 40000 },
+        {id: 2, name: 'mac book', price: 80000}
+    ]
+}
+
+console.log(products.data[1].name);
+
