c1858fdc668dc665958c43c0e630578538111eca
mdNaeemUddin <md.naeem_uddin98@yahoo.com>
2024-02-25 23:12:21 +0600

    moduel 32 practicing

---
+++ types.js
@@ -0,0 +1,21 @@
+// data types -
+// int , string, bool, object, array, string
+
+
+//primitive types value 
+
+const a = 5;
+const b = 'a string value';
+const c = true;
+
+
+//non-primitive types value
+
+const age = [23, 32, 98];
+const student = {
+    id: 32,
+    name: 'Naeem',
+    age: 23
+}
+
+console.log(typeof a, typeof b,typeof c,typeof age, typeof student);
