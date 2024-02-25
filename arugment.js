e3a7c84950e5061ec30b43414cc93dd0822b3bcf
mdNaeemUddin <md.naeem_uddin98@yahoo.com>
2024-02-26 02:00:42 +0600

    module 32 practice done

--- arugment.js
+++ arugment.js
@@ -1,5 +1,8 @@
 // arguments
 
+// object and array are pass by reference
+// primitive types are pass by value
+
 function sum(a, b, c) {
     console.log(arguments[4]);
     console.log(typeof arguments);
@@ -7,3 +10,8 @@ function sum(a, b, c) {
 }
 
 console.log(sum(2, 4, 5, 88, 23));
+
+let a = 'some';
+let b = a;
+b = 'react';
+console.log(b);
